677c7116668fe3283d11b91e4401834dc1d2005e
Arthur Le Meur <arthur@edrlab.org>
2023-06-23 15:36:39 +0200

    display selected filters

--- static/js/filter-categories.js
+++ static/js/filter-categories.js
@@ -3,3 +3,23 @@ const dropdown = document.querySelectorAll('.dropdown');
 dropdown.forEach(input => input.addEventListener('click', function() {
     input.classList.toggle('active')
 }));
+
+function getCurrentURL () {
+    return window.location.href
+}
+
+
+const url = getCurrentURL();
+const splitUrl = url.split(/[?=]+/);
+
+
+const container = document.querySelector('.filter-display');
+
+if (splitUrl[1] === undefined) {
+    container.style.diplay = "none";
+} else {
+    const filter = splitUrl[1];
+    const parameter = splitUrl[2].replace(/%20/g, " ");
+    container.innerText = `${filter} : ${parameter}`;
+}
+
